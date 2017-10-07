b172dd88611edcd4a2f357c618a03f74d551e29c
dzuvan <hrnjak.jovan1@gmail.com>
2017-10-07 19:08:51 +0200

    Added window icon.

--- Tangram/Game.cpp
+++ Tangram/Game.cpp
@@ -6,41 +6,49 @@
 #include "PlayState.h"
 #include "MainMenuState.h"
 #include "SoundManager.h"
+#include "TextureManager.h"
 
 Game* Game::s_pInstance = 0;
 
-bool Game::init(const char *title, int xpos, int ypos, int width, int height, int flags){
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)  == 0) {
-        std::cout<<"Subsystem initialization success\n"<<std::endl;
+bool Game::init(const char *title, int xpos, int ypos, int width, int height, int flags) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) == 0) {
+        std::cout << "Subsystem initialization success\n" << std::endl;
         m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 
         if (m_pWindow != 0) {
-            std::cout<<"Window created\n"<<std::endl;
+            std::cout << "Window created\n" << std::endl;
+
+            SDL_Surface* surface = IMG_Load("assets/tangram.png");
+            SDL_SetWindowIcon(m_pWindow, surface);
+
             m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 
             if (m_pRenderer != 0) {
-                std::cout<<"Renderer created\n";
+                std::cout << "Renderer created\n";
                 SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
-            } else {
-                std::cout<<"Render initialization failed\n"<<std::endl;
+            }
+            else {
+                std::cout << "Render initialization failed\n" << std::endl;
                 return false;
             }
-        } else {
-            std::cout<<"Window initialization failed\n"<<std::endl;
+        }
+        else {
+            std::cout << "Window initialization failed\n" << std::endl;
             return false;
         }
-    } else {
-        std::cout<<"SDL Initialization failed\n"<<std::endl;
+    }
+    else {
+        std::cout << "SDL Initialization failed\n" << std::endl;
         return false;
     }
 
     m_pGameStateMachine = new GameStateMachine();
     m_pGameStateMachine->changeState(new MainMenuState());
 
-    SoundManager::Instance()->load("assets/click-sound.wav","1", sound_type(1));
-    SoundManager::Instance()->load("assets/victory.ogg","2", sound_type(0));
+    SoundManager::Instance()->load("assets/click-sound.wav", "1", sound_type(1));
+    SoundManager::Instance()->load("assets/victory.ogg", "2", sound_type(0));
 
-    std::cout<<"Initialization complete"<<std::endl;
+    std::cout << "Initialization complete" << std::endl;
     m_bRunning = true;
     return true;
 }
@@ -55,14 +63,14 @@ void Game::render() {
 }
 
 void Game::update() {
-      m_pGameStateMachine->update();
+    m_pGameStateMachine->update();
 }
 
 
 void Game::handleEvents() {
     InputHandler::Instance()->update();
     if (InputHandler::Instance()->isKeyDown(SDL_SCANCODE_RETURN)) {
-		m_pGameStateMachine->changeState(new PlayState());
+        m_pGameStateMachine->changeState(new PlayState());
     }
 }
 
